22acaf82d9e3aeab5a939d2f9cdaefa30d6ba054
caojianmin <46328885+caojianmin@users.noreply.github.com>
2019-01-03 14:55:06 +0800

    Revert "Initial commit"
    This reverts commit 55a6750d96828f7d065a8de8a9e2a477b3669111.

--- MyLotterProject/ReactDemo/LotterDemo/src/lottery.js
+++
@@ -1,4 +0,0 @@
-import web3 from './web3';
-const address = '0x78922e6a9e3A1BA33dEB167C4d15636C8bFB5bC2';
-const abi = [{"constant":true,"inputs":[],"name":"manager","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[],"name":"getPlayers","outputs":[{"name":"","type":"address[]"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[],"name":"enetr","outputs":[],"payable":true,"stateMutability":"payable","type":"function"},{"constant":false,"inputs":[],"name":"pickwiner","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"players","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"inputs":[],"payable":false,"stateMutability":"nonpayable","type":"constructor"}];
-export default new web3.eth.Contract(abi,address);
